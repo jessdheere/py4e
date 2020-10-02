0936fbf6b61ef62808d599b7ba87eb8d0c7f9985
Rronny01 <72240947+Rronny01@users.noreply.github.com>
2020-10-02 12:40:27 +0530

    Pay in Float

--- py_ch2/chapter2ex3.py
+++ py_ch2/chapter2ex3.py
@@ -3,7 +3,5 @@ name = raw_input ("Hi, there. What's your name?\n")
 print "Hello, "+name+"!"
 hours = input ("How many hours did you work this week?\n") #use input (not raw input) when expecting numbers
 rate = input ("What's your hourly rate?\n") #use input (not raw input) when expecting numbers
-pay = hours * rate
+pay = float(hours * rate)
 print pay
-
-
